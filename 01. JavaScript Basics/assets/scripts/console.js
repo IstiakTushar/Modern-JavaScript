dc141f7df7e18272886da303acb858d206005a93
IstiakTushar <istiak.tushar742@gmail.com>
2020-03-08 23:18:15 +0600

    Log to console

---
+++ 01. JavaScript Basics/assets/scripts/console.js
@@ -0,0 +1,20 @@
+// Log to Console
+console.log('Hello Rubaiyat');
+console.log(1,2,3);
+console.log(true);
+var greeting = 'Hello Ramanabaiyat';
+console.log(greeting);
+console.log([1, 2, 3,4, 5]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+console.error('This is an error!');
+console.clear();
+console.warn('This is just a warning message');
+console.time('Hello Rubaiyat');
+    console.log(greeting);
+    console.log(greeting);
+    console.log(greeting);
+    console.log(greeting);
+    console.log(greeting);
+    console.log(greeting);
+console.timeEnd('Hello Rubaiyat');
